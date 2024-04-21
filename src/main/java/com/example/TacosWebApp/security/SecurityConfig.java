78c42cc83f2c3a73acb666a80785a77fddcf8701
Iovane <iovane.chikovani.1@btu.edu.ge>
2024-04-21 22:05:11 +0400

    add access-denied.html

--- src/main/java/com/example/TacosWebApp/security/SecurityConfig.java
+++ src/main/java/com/example/TacosWebApp/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -35,35 +36,22 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/register", "/login").permitAll()
-                        .requestMatchers("/design", "/orders", "/orders/current").hasRole("USER")
+                        .requestMatchers("/design", "/orders", "/orders/current", "/recent").hasRole("USER")
+                        .requestMatchers("/recent/delete", "/recent/delete/**").hasRole("ADMIN")
                         .requestMatchers("/", "/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().authenticated())
 
-                )
                 .formLogin(form -> form
                         .loginPage("/login")
                         .successHandler(customAuthenticationSuccessHandler)
-                        .permitAll());
+                        .permitAll())
+
+                .logout(LogoutConfigurer::permitAll)
+
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied"));
 
 
         return http.build();
     }
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
-//    }
-
-//    @Bean
-//    @Primary
-//    public AuthenticationManagerBuilder userConfigure(AuthenticationManagerBuilder auth) throws Exception {
-//        BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
-//        auth.inMemoryAuthentication()
-//                .withUser("Kong")
-//                .password(bcrypt.encode("12345"))
-//                .roles("USER")
-//                .and()
-//                .passwordEncoder(bcrypt);
-//        return auth;
-//    }
 }
